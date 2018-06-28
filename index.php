af17a14bef1f4d56fc64517c5e0ffa40cdc4b9a4
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-06-29 07:37:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,42 +17,23 @@ for($j=$i;$j>=1;$j--){
 
 
 
-4)
+2)
 <?php
  echo "<br />";
 ?>
 
 <?php 
-for($i=5;$i>=1;$i--){
-for($j=5;$j>$i;$j--){
+for($i=5;$>=1;$i--){
+for($j=1;$j<$i;$j++){
  
        echo "  ";
 }
- for($k=1;$k<=$i;$k++){
+ for($k=5;$k>=$i;$k--){
   echo " * ";
  
  }
        echo "<br />";
      }
-
-4)
-<?php
- echo "<br />";
-?>
-
-<?php 
-for($i=5;$i>=1;$i--){
-for($j=5;$j>$i;$j--){
- 
-       echo "  ";
-}
- for($k=1;$k<=$i;$k++){
-  echo " * ";
- 
- }
-       echo "<br />";
-     }
-
 
 
 
