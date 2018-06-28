5bdd3dec10bc6cdb568c095f715b06be19628e69
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-06-29 06:10:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
 <?php
-echo "<pre>";
-for ($i = 1; $i < 8; $i++) {
-    for ($j = $i; $j < 8; $j++)
-        echo "&nbsp;&nbsp;";
-    for ($j = 2 * $i - 1; $j > 0; $j--)
-        echo ("&nbsp;*");
-    echo "<br>";
+
+for ($i = 0; $i <5; $i++) {
+    for ($j = 0; $j < $i; $j++){
+   echo " * ";
 }
-$n = 8;
-for ($i = 8; $i > 0; $i--) {
-    for ($j = $n - $i; $j > 0; $j--)
-        echo "&nbsp;&nbsp;";
-    for ($j = 2 * $i - 1; $j > 0; $j--)
-        echo ("&nbsp;*");
-    echo "<br>";
+
+    echo "<br />";
 }
-echo "</pre>";
-?>
+
 
 <?php 
 for($i=1;$i<=6;$i++)
