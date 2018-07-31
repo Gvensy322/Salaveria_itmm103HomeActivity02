627d31a4caa92a230bbbc918b2560a6cebbc46b9
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-07-31 11:25:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,48 @@
-1)
 <?php
- echo "<br />";
-?>
 
-<?php 
-for($i=1;$i<=5;$i++)
-{
-for($j=$i;$j>=1;$j--)
-{ 
-       echo " * ";
+echo "1)";
+echo "<br>";
+for ($x = 0; $x <= 5; $x++){
+	for ($y = 0; $y < $x; $y++){
+		echo " * ";
+	}
+	echo "<br>";
 }
-       echo "<br />";
-     }
-  
- 
-?>
-
-3)
-<?php
- echo "<br />";
-?>
-
-<?php 
-for($i=6;$i>=1;$i--){
-for($j=1;$j<$i;$j++){
- 
-       echo " * ";
+echo "<br> <br>";
+echo "2)";
+echo "<br><br>";
+for ($x = 5; $x > 0; $x--){
+	for ($y = 0; $y < $x; $y++){
+		echo "&nbsp &nbsp";
+	}
+	
+	for ($z = 5; $z >= $x; $z--){
+		echo " * ";
+	}
+	echo "<br>";
 }
-       echo "<br />";
-     }
-  
- 
+echo "<br> <br>";
+echo "3)";
+echo "<br> <br>";
+for ($x = 5; $x >= 0; $x--){
+	for ($y = 0; $y < $x; $y++){
+		echo " * ";
+	}
+	echo "<br>";
+}
+echo "4)";
+echo "<br><br>";
+for ($x = 5; $x >= 0; $x--){
+	for($y = 5; $y > $x; $y--){
+		echo "&nbsp &nbsp";
+	}
+		for ($z = 0; $z < $x; $z++){
+			echo " * ";
+		}
+		echo "<br>";
+}
 ?>
+
 
 
 
