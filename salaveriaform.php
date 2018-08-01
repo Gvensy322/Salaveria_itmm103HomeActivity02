317beacc013e30229e80308ae827d92374d12d0f
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-08-02 06:15:53 +0800

    Create salaveriaform.php

---
+++ salaveriaform.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+
+
+<p>
+
+
+Welcome <br>
+Name: <?php echo $_POST["name"]; ?>
+<br>
+Date of Birth: <?php echo $_POST["dateofbirth"] ?>
+<br>
+Address : <?php echo $_POST["address"]; ?>
+<br>
+
+Email : <?php echo $_POST["email"]; ?>
+<br>
+
+
+
+</p>
+</body>
+</html>
